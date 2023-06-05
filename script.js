1c9ad0c13551323ab3e0ebe04cc72d7a8de2b2e1
Tayyab Haseeb <73613576+tayyabhaseeb@users.noreply.github.com>
2023-06-05 21:29:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,112 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+// Variables Declared
+let passwordOne = document.getElementById("password-one");
+let passwordTwo = document.getElementById("password-two");
+const copyButton = document.getElementsByClassName("copyButton");
+let limit = "true";
+// functionality
+
+function generate() {
+  if (limit) {
+    for (let i = 0; i < 15; i++) {
+      let randomNum1 = Math.floor(Math.random() * characters.length);
+      let randomNum2 = Math.floor(Math.random() * characters.length);
+      passwordOne.textContent += characters[randomNum1];
+      passwordTwo.textContent += characters[randomNum2];
+    }
+  }
+  limit = false;
+}
